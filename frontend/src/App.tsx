054fa49f2172ff7640aa7363e8511e30eafa49b8
João Gabriel Dos Santos <joaog.tijuca@gmail.com>
2026-09-10 18:49:48 -0300

    feat: add about section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from "./components/Navbar/Navbar";
 import { Hero } from "./sections/Hero/Hero";
+import { About } from "./sections/About/About";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
 
       <main>
         <Hero />
+        <About />
       </main>
     </>
   );
