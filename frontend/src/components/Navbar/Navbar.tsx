34f4ff98e1a60390ea06173668cc447c9c10b24f
João Gabriel Dos Santos <joaog.tijuca@gmail.com>
2026-09-10 17:11:31 -0300

    feat: add portfolio navigation bar

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -5,30 +5,16 @@ export function Navbar() {
     <header className="navbar">
       <div className="container navbar__content">
         <a href="#home" className="navbar__logo">
-          Nome
+          Seu Nome
         </a>
 
         <nav>
           <ul className="navbar__links">
-            <li>
-              <a href="#home">Início</a>
-            </li>
-
-            <li>
-              <a href="#about">Sobre</a>
-            </li>
-
-            <li>
-              <a href="#skills">Skills</a>
-            </li>
-
-            <li>
-              <a href="#projects">Projetos</a>
-            </li>
-
-            <li>
-              <a href="#contact">Contato</a>
-            </li>
+            <li><a href="#home">Início</a></li>
+            <li><a href="#about">Sobre</a></li>
+            <li><a href="#skills">Skills</a></li>
+            <li><a href="#projects">Projetos</a></li>
+            <li><a href="#contact">Contato</a></li>
           </ul>
         </nav>
       </div>
